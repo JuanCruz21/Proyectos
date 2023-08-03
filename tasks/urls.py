e2eaca5b53c1f7a15ab0648f42cac9dd41186084
JuanCruz21 <82663723+JuanCruz21@users.noreply.github.com>
2023-08-03 14:12:23 -0500

    Cambios

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUp)
+    path('', views.home,name='home'),
+    path('signup/', views.SignUp, name='SignUp'),
+    path('login/', views.login,name='login'),
+    path('tasks', views.tasks, name='tasks')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,18 +2,37 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 # Create your views here.
+
+def home(request):
+    empresa = 'Maggio'
+    return render(request, 'Home.html', {'empresa': empresa})
 
 def SignUp(request):
     if request.method == 'GET':
-        print (request.POST)
+        print(request.POST)
         return render(request, 'SignUp.html', {
-            'signup':UserCreationForm
+            'signup': UserCreationForm
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
-           user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
-           user.save()
-           print(request.POST)
-           return HttpResponse('Se creo exitosamente')
-        return HttpResponse('No se creo exitosamente')
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user) 
+                return render(request, 'tasks.html')
+            except IntegrityError:
+                return render(request, 'tasks.html')
+        else:
+            return render(request, 'SignUp.html', {
+                'signup': UserCreationForm, 'Error': 'Las contraseñas no coinciden'
+            })
+    
+def login(request):
+    return render(request, 'Login.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
