1b6ba15a09a83ce68dceb03c5459767ad2dd96ab
JuanCruz21 <juancruz.02.2001@gmail.com>
2023-08-08 22:34:56 -0500

    Cambios

--- tasks/views.py
+++ tasks/views.py
@@ -68,8 +68,19 @@ def Newtasks(request):
         return redirect('tasks')
     
 def DetailTask(request,task_id):
-    task = get_object_or_404(Task,pk=task_id)
-    return render(request, 'taskdetail.html', {'task':task})
+    if request.method == 'GET':
+        task = get_object_or_404(Task,pk=task_id)
+        form = TaskForm(instance=task)
+        return render(request, 'taskdetail.html', {'task':task,'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task,pk=task_id)
+            form = TaskForm(request.POST,instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'taskdetail.html', {'task':task,'form': form, 
+                                                       'Error': "No se pudo actualizar"})
 
 def signout(request):
     logout(request)
